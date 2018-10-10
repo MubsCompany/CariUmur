866401e20bcc8bdcb36b5b10bea24ec41a2caf23
Obed D <obed@obeds-macbook-pro.local>
2018-10-10 10:39:30 +0700

    Initial Commit

---
+++ CariUmur/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CariUmur
+//
+//  Created by Obed RPL D on 08/10/18.
+//  Copyright © 2018 Syams.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var txtUmur: UITextField!
+    @IBOutlet weak var lblHasil: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func btnUmur(_ sender: Any) {
+        let tahunlahir = Int(txtUmur.text!)
+        let tahunsekarang = 2018 - tahunlahir!
+        //menampilkan pada text field
+        lblHasil.text = "Umur kamu adalah \(tahunsekarang)"
+    }
+    
+    
+}
+
